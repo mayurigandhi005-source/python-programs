377ba58d59fcae4b88f7ccaeb0ce5e9f0b351353
mayurigandhi005-source <mayurigandhi005@gmail.com>
2026-09-15 20:15:44 +0530

    Speak

--- speaking.py
+++ speaking.py
@@ -1,4 +1,7 @@
 import pyttsx3
 engine = pyttsx3.init()
-engine.say("hello..! I'm Mayuri Gandhi ")
+engine.say('''Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky. ''')
 engine.runAndWait()
