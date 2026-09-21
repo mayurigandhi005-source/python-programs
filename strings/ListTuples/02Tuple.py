47ac8fe5692c4f08b626008181ecf4f75d692f24
mayurigandhi005-source <mayurigandhi005@gmail.com>
2026-09-21 14:35:39 +0530

    Add Pattern

---
+++ strings/ListTuples/02Tuple.py
@@ -0,0 +1,10 @@
+a = (1,"apple",False,"radha",78,78)
+print(a)
+
+no = a.count(78)
+print(no)
+
+co = a.index(False)
+print(co)
+
+print(len(a))
---
+++ strings/letter.py
@@ -0,0 +1,6 @@
+letter = ''' Dear <|name|>,
+you are selected..!
+<|Date|>
+'''
+
+print (letter.replace("<|name|>" , "Mayuri").replace("<|Date|>","20 sep 2026 "))
